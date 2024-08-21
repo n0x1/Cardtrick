9378e2d8008eea98dba7e6a945b5949a0e1f5cf9
Cam <cameronmanzo111@gmail.com>
2024-08-20 23:04:00 -0400

    gamestate basic done

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -1,11 +1,13 @@
 @tool
 class_name Character extends Node2D
 
-@export var max_health: int = 100
-@export var health: int = 100
+@export var sprite_texture: CompressedTexture2D
+@export var max_health: int = 25
+@export var health: int = 25
 @export var shield: int = 0
-@export var mana: int = 5
-
+@export var mana: int = 3
+@export var current_mana_cap: int = 3
+@export var damage_change: int = 0 # positive indicates buff and negative nerfs
 func spend_mana(amount: int):
 	if mana - amount >= 0:
 		mana -= amount
@@ -15,19 +17,20 @@ func spend_mana(amount: int):
 		#display some message or error sound
 
 func take_damage(amount: int):
-	if shield > 0: # dmg first goes to shield
-		shield -= amount
-		if shield < 0:
-			health += shield # shield will be negative so +
-			shield = 0
-	else:
-		health -= amount
+	if amount > 0:
+		if shield > 0: # dmg first goes to shield
+			shield -= amount
+			if shield < 0:
+				health += shield # shield will be negative so +
+				shield = 0
+		else:
+			health -= amount
 
 func change_shield(amount: int):
 	shield += amount
 
 func change_attack(amount: int):
-	# attack += amount
+	damage_change += amount # use negatives to debuff
 	pass
 
 
@@ -43,11 +46,29 @@ func update_healthbar(): # hp bar graphical update only
 		($Healthbar as ProgressBar).max_value = max_health 
 	if ($Healthbar as ProgressBar).value != health:
 		($Healthbar as ProgressBar).value = health
+	if health < 0:
+		health = 0 #(for consistency with visuals)
 	($"Healthbar/HealthText").set_text(str(health) + "/" + str(max_health))
 
 func update_shield_icon_values():
 	$"Shield".visible = shield > 0 # returns true or false
 	$"Shield/ShieldNum".set_text(str(shield))
+	$"AttackChange".visible = damage_change != 0 
+	if damage_change > 0:
+		$"AttackChange/AttackChgNum".set_text("+" + str(damage_change))
+	else:
+		$"AttackChange/AttackChgNum".set_text(str(damage_change))
+	
+	
+func start_turn():
+	if shield > 0: 
+		shield -=1 # decay so balanced ig
+	if damage_change > 0: #decay
+		damage_change -= 1
+	elif damage_change < 0:
+		damage_change += 1
+	mana = current_mana_cap
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -57,4 +78,5 @@ func _ready():
 func _process(delta):
 	update_healthbar()
 	update_shield_icon_values()
+	
 
--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,6 +3,9 @@ extends Node2D
 @export var player_character: Character
 
 @onready var game_control: GameController = $GameController
+
+var enemy_character_state: int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,8 +14,37 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$ManaAmount.set_text(str($GameScreen/PlayerCharacter.mana))
+	
 	if $GameScreen/PlayerCharacter.health <= 0:
-		print("game over")
+		game_control.transition(GameController.GameState.GAMEOVER)
+	elif $GameScreen/EnemyCharacter.health <= 0:
+		game_control.transition(GameController.GameState.VICTORY)
+	
+	if game_control.current_state == GameController.GameState.ENEMY_TURN:
+		#ai logic
+		var enemydamagechange = $GameScreen/EnemyCharacter.damage_change
+		if enemy_character_state == 0:
+			$GameScreen/EnemyCharacter.change_shield(3)
+		elif enemy_character_state == 1:
+			$GameScreen/PlayerCharacter.take_damage(2 + enemydamagechange)
+		elif enemy_character_state == 2:
+			$GameScreen/PlayerCharacter.take_damage(4 + enemydamagechange)
+			
+		enemy_character_state = posmod(enemy_character_state + 1, 3) # % but +
+		game_control.transition(GameController.GameState.PLAYER_TURN)
+		$GameScreen/PlayerCharacter.start_turn()
+		
+	if game_control.current_state == GameController.GameState.VICTORY:
+		$CanvasLayer/VictoryOverlay.visible = true
+		print("Victory")
+	else:
+		$CanvasLayer/VictoryOverlay.visible = false
+		
+	if game_control.current_state == GameController.GameState.GAMEOVER:
+		$CanvasLayer/GameOverOverlay.visible = true
+		print("Gameover")
+	else:
+		$CanvasLayer/GameOverOverlay.visible = false
 		# add actual mechanic so they acnt play
 
 
@@ -43,3 +75,9 @@ func _on_inflict_1_button_pressed():
 func _on_inflict_3_button_pressed():
 	player_character.take_damage(3)
 
+
+
+func _on_end_turn_pressed():
+	if game_control.current_state == GameController.GameState.PLAYER_TURN:
+		game_control.transition(GameController.GameState.ENEMY_TURN)
+		$GameScreen/EnemyCharacter.start_turn()
